9e238cb74c6bbc97f9114c5760c6251c3c709355
ngotwaaron <ngotwaaron@gmail.com>
2024-03-25 15:17:11 +0300

    complete scope lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+// Write your solution in this file!
+var customerName = "bob";
+console.log(customerName);
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+    return customerName;
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'John Doe';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'Jane Smith'
+}
